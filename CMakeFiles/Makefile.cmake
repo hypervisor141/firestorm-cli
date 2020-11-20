544ccc6d59747a4821f9cb01f4896602e4186d8f
Nurverek <nurverek@gmail.com>
2020-11-20 15:40:27 +0330

    minor changes to CMake

--- CMakeFiles/Makefile.cmake
+++ CMakeFiles/Makefile.cmake
@@ -7,9 +7,9 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "CMakeFiles/3.18.4/CMakeCCompiler.cmake"
-  "CMakeFiles/3.18.4/CMakeCXXCompiler.cmake"
-  "CMakeFiles/3.18.4/CMakeSystem.cmake"
+  "CMakeFiles/3.18.5/CMakeCCompiler.cmake"
+  "CMakeFiles/3.18.5/CMakeCXXCompiler.cmake"
+  "CMakeFiles/3.18.5/CMakeSystem.cmake"
   "CMakeLists.txt"
   "/usr/share/cmake-3.18/Modules/CMakeCInformation.cmake"
   "/usr/share/cmake-3.18/Modules/CMakeCXXInformation.cmake"
